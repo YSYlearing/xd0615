9692fa2e52338036c5923fc82f45a9a2b06d537b
YSY <ysy000719@126.com>
2020-06-18 21:51:47 +0800

    提交第三次作业by姚诗懿 19140100228

--- lessons 3/zhuanlun.c
+++ lessons 3/zhuanlun.c
@@ -2,10 +2,10 @@ char ch=' ';
 void setup()
 {
     Serial.begin(9600);
-    pinMode(6,OUTPUT);
-    pinMode(7,OUTPUT);
-    pinMode(8,OUTPUT);
-    pinMode(9,OUTPUT);
+    pinMode(4,OUTPUT);
+    pinMode(5,OUTPUT);
+    pinMode(10,OUTPUT);
+    pinMode(11,OUTPUT);
   Serial.println("ok");
 }
 /*
@@ -23,34 +23,34 @@ void loop()
     switch(ch)
     {
       case'b':
-       digitalWrite(6,HIGH);
-       digitalWrite(7,LOW);
-       digitalWrite(8,HIGH);
-       digitalWrite(9,LOW);
+       digitalWrite(4,HIGH);
+       digitalWrite(5,LOW);
+       digitalWrite(10,HIGH);
+       digitalWrite(11,LOW);
       break;
       case'f':
-       digitalWrite(7,HIGH);
-       digitalWrite(6,LOW);
-       digitalWrite(9,HIGH);
-       digitalWrite(8,LOW);
+       digitalWrite(5,HIGH);
+       digitalWrite(4,LOW);
+       digitalWrite(11,HIGH);
+       digitalWrite(10,LOW);
       break;
       case'l':
-       digitalWrite(7,HIGH);
-       digitalWrite(6,LOW);
-       digitalWrite(8,HIGH);
-       digitalWrite(9,LOW);
+       digitalWrite(5,HIGH);
+       digitalWrite(4,LOW);
+       digitalWrite(10,HIGH);
+       digitalWrite(11,LOW);
       break;
       case'r':
-       digitalWrite(6,HIGH);
-       digitalWrite(7,LOW);
-       digitalWrite(9,HIGH);
-       digitalWrite(8,LOW);
+       digitalWrite(4,HIGH);
+       digitalWrite(5,LOW);
+       digitalWrite(11,HIGH);
+       digitalWrite(10,LOW);
       break;
       case's':
-       digitalWrite(6,LOW);
-       digitalWrite(7,LOW);
-       digitalWrite(8,LOW);
-       digitalWrite(9,LOW);
+       digitalWrite(4,LOW);
+       digitalWrite(5,LOW);
+       digitalWrite(10,LOW);
+       digitalWrite(11,LOW);
       default:
       break;
     }
